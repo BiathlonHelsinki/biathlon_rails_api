bcb41ccf0a07456a48ccdf187c0f44321567b962
John W. Fail <john@pixelache.ac>
2016-09-07 11:58:09 +0300

    upload base64 image fix

--- app/controllers/instances_controller.rb
+++ app/controllers/instances_controller.rb
@@ -57,9 +57,45 @@ class InstancesController < ApplicationController
   private
   
   def instance_params
-    params.require(:instance).permit(:published, :event_id, :place_id, :primary_sponsor_id, :is_meeting, :proposal_id,
+    
+    the_params = params.require(:instance).permit(:published, :event_id, :place_id, :primary_sponsor_id, :is_meeting, :proposal_id,
     :secondary_sponsor_id, :cost_euros, :cost_bb,  :sequence, :start_at, :end_at, :sequence, :allow_multiple_entry,
     :parent_id, :image, translations_attributes: [:name, :description, :locale, :id]
     )
+    the_params[:image] = parse_image_data(the_params[:image]) if the_params[:image]
+    the_params
   end  
+  
+
+  def parse_image_data(base64_image)
+    filename = "upload-image"
+    # in_content_type, encoding, string = base64_image.split(/[:;,]/)[1..3]
+    # logger.warn('string is ' + string.inspect)
+    @tempfile = Tempfile.new(filename)
+    @tempfile.binmode
+    @tempfile.write Base64.decode64(base64_image)
+    @tempfile.rewind
+
+    # for security we want the actual content type, not just what was passed in
+    content_type = `file --mime -b #{@tempfile.path}`.split(";")[0]
+
+    # we will also add the extension ourselves based on the above
+    # if it's not gif/jpeg/png, it will fail the validation in the upload model
+    extension = content_type.match(/gif|jpeg|png/).to_s
+    filename += ".#{extension}" if extension
+
+    ActionDispatch::Http::UploadedFile.new({
+      tempfile: @tempfile,
+      content_type: content_type,
+      filename: filename
+    })
+  end
+
+  def clean_tempfile
+    if @tempfile
+      @tempfile.close
+      @tempfile.unlink
+    end
+  end
+  
 end
