6782a488fed163b599371cd3cc2da35622bf2ebd
John W. Fail <john@pixelache.ac>
2016-11-10 10:40:05 +0200

    paranoid pledges

--- app/models/pledge.rb
+++ app/models/pledge.rb
@@ -1,5 +1,6 @@
 class Pledge < ApplicationRecord
   belongs_to :item, polymorphic: true
+  acts_as_paranoid
   belongs_to :user
   validates_presence_of :user_id, :pledge
   validates_numericality_of :pledge, greater_than_or_equal_to: 0
