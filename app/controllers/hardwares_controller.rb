942354015a676b539ec022968341174a634404b7
John W. Fail <john@pixelache.ac>
2017-03-14 12:59:43 +0200

    clear notification

--- app/controllers/hardwares_controller.rb
+++ app/controllers/hardwares_controller.rb
@@ -5,6 +5,7 @@ class HardwaresController < ApplicationController
   def i_am_alive
     if current_hardware.checkable?
       current_hardware.update_attribute(:last_checked_at, Time.current)
+      current_hardware.update_attribute(:notified_of_error, false)
     end
   end
 end
