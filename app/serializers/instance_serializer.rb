85fae595e69b12bf8eab55a1a88363a42801d4c0
John W. Fail <john@pixelache.ac>
2017-04-27 11:29:02 +0300

    trying to fix timezones

--- app/serializers/instance_serializer.rb
+++ app/serializers/instance_serializer.rb
@@ -1,7 +1,14 @@
 class InstanceSerializer < ActiveModel::Serializer
   attributes :id, :name, :start_at, :end_at, :description, :cost_bb, :image, :slug, :checked_in_so_far
 
-
+  def start_at 
+    object.start_at.localtime
+  end
+  
+  def end_at
+    object.end_at.blank? ? nil : object.end_at.localtime
+  end
+  
   def checked_in_so_far
     object.instances_users.where(visit_date: Time.current.to_date).size + object.onetimers.today(Time.current.to_date).size
   end
