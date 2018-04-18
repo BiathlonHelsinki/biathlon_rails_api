406b4872b075ff27aa426bee95ac1a4cf3ab9a25
John W. Fail <john@pixelache.ac>
2018-04-18 14:54:57 +0300

    point transfering fix

--- app/controllers/transfers_controller.rb
+++ app/controllers/transfers_controller.rb
@@ -11,41 +11,18 @@ class TransfersController < ApplicationController
     end
     sender = params[:from_account]
 
-    # check if recipient has ethereum account yet
-    if recipient.accounts.empty?
-      create_call = HTTParty.post(Figaro.env.dapp_address + '/create_account',
-                                   body: {password: recipient.geth_pwd})
-      unless JSON.parse(create_call.body)['data'].blank?
-        recipient.accounts << Account.create(address: JSON.parse(create_call.body)['data'])
-      end
-    end
+
     if params[:userphoto_id] != ''
       userphoto = Userphoto.find(params[:userphoto_id]) 
     end
     
-    # account is created in theory, so now let's do the transaction
-    # api = BidappApi.new
+ 
     begin
       # if sender.accounts.primary.first.external == true
 
        b = BlockchainTransaction.new(value: params[:points], 
-          account: Account.find_by(address: sender), recipient: recipient.accounts.first,  
+          account: Account.find_by(address: sender), recipient: Account.find_by(address: recipient.get_eth_address),  
           transaction_type: TransactionType.find_by(name: 'Transfer'))
-        # transaction = api.transfer(sender.accounts.first.address, recipient.accounts.first.address, params[:points])
-      # else
-        # transaction = api.transfer(sender.accounts.first.address, recipient.accounts.first.address, params[:points])
-         #, sender.geth_pwd)
-      # end
-      # if JSON.parse(transaction)['error']
- #        render json: {error: JSON.parse(transaction)['error']}, status: 400
- #      else
- #        sleep 2
-        # et = nil
-        # loop do
-          # et = Ethtransaction.find_by(txaddress: JSON.parse(transaction)['data'])
-        #   sleep 1
-        #   break if !et.nil?
-        # end
 
         a = Activity.create(user: current_user, item: recipient,  
                             contributor: Account.find_by(address: sender).holder,
